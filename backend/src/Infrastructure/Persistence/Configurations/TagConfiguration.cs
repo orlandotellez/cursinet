de6bc67cdede15a83cb62f4f1ae658261e799452
destripador2000 <davidhumbertop503@gmail.com>
2026-06-01 20:00:10 -0600

    Feat: Creación de la configuración de todas las columnas del modelo tag

---
+++ backend/src/Infrastructure/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Cursinet.Domain.Entities;
+
+namespace Cursinet.Infrastructure.Persistence.Configurations;
+
+public class TagConfiguration : IEntityTypeConfiguration<Tag>
+{
+	public void Configure(EntityTypeBuilder<Tag> builder)
+	{
+		// Nombre que le daremos a la tabla
+		builder.ToTable("Tags");
+
+		// Configuraciones de los campos de la tabla Tags
+		builder.HasKey(t => t.Id);
+		builder.Property(t => t.Id).HasColumnName("id").HasDefaultValueSql("uuid_generate_v4()");
+
+		builder.Property(t => t.Name).IsRequired().HasColumnName("name").HasMaxLength(100);
+
+		builder.Property(t => t.Slug).IsRequired().HasColumnName("slug").HasMaxLength(100);
+		builder.HasIndex(t => t.Slug).IsUnique();
+
+		builder.Property(t => t.CreatedAt).IsRequired().HasColumnName("created_at").HasDefaultValueSql("CURRENT_TIMESTAMP");
+		builder.Property(t => t.UpdatedAt).IsRequired().HasColumnName("updated_at").HasDefaultValueSql("CURRENT_TIMESTAMP");
+	}
+}
