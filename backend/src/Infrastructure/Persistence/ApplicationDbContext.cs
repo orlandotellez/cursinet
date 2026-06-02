21362d684c360a46fd7cd4cb5cc18b550c36af1e
destripador2000 <davidhumbertop503@gmail.com>
2026-06-01 20:06:17 -0600

    Feat: Agregando modelo tag al dbcontext para que al momento de hacer la migración se cree la tabla en la bd

--- backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<UserTwoFactor> UserTwoFactor => Set<UserTwoFactor>();
     public DbSet<LoginLogs> LoginLogs => Set<LoginLogs>();
     public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Tag> Tags => Set<Tag>();
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
