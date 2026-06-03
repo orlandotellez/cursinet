e755077a64816a7c30208732ca26fcae844e998a
destripador2000 <davidhumbertop503@gmail.com>
2026-06-03 07:27:33 -0600

    Feat: Agregando modelo LessonProgress para que al momento de hacer la migración se cree la tabla en la bd

--- backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -24,6 +24,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Module> Modules => Set<Module>();
     public DbSet<Lesson> Lessons => Set<Lesson>();
     public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+    public DbSet<LessonProgress> LessonProgress => Set<LessonProgress>();
     public DbSet<Payment> Payments => Set<Payment>();
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
