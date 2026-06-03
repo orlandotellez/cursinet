429dfdb69d2eacac17e7d73fd11501fbca847a6d
destripador2000 <davidhumbertop503@gmail.com>
2026-06-02 21:00:13 -0600

    Feat: Agregando modelo payment al dbcontext para que al momento de hacer la migración se cree la tabla

--- backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -23,6 +23,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<CourseTag> CourseTags => Set<CourseTag>();
     public DbSet<Module> Modules => Set<Module>();
     public DbSet<Lesson> Lessons => Set<Lesson>();
+    public DbSet<Payment> Payments => Set<Payment>();
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
