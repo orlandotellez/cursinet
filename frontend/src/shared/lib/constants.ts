d1dc20f991b18af74e58af32debb816c09f7a7ca
orlandotellez <espadachinkinno@gmail.com>
2026-06-27 15:50:14 -0600

    chore(config): update globals.css, next.config.ts, constants.ts; add useDebounce lib helper

--- frontend/src/shared/lib/constants.ts
+++ frontend/src/shared/lib/constants.ts
@@ -1,4 +1,7 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
+export const API_URL =
+  process.env.API_URL ??
+  process.env.NEXT_PUBLIC_API_URL ??
+  'http://localhost:5000/api/v1';
 
 export const PLYR_DEFAULT_OPTIONS: Partial<Plyr.Options> = {
   controls: [
